525b201000f10414e34ea0388a349452dfc0b2fe
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-06 04:27:45 +0900

    描画用関数のファイルを作って再挑戦

---
+++ src/draw.js
@@ -0,0 +1,10 @@
+function drawTest(){
+  var ctx = getctx();
+  ctx.drawImage(blank, 0, 0);
+  ctx.beginPath();
+  ctx.arrow(200, 400, 200, 0, [0, 1, -10, 1, -10, 5]);
+  ctx.arrow(0, 200, 400, 200, [0, 1, -10, 1, -10, 5]);
+  ctx.fillStyle = "#000";
+  ctx.fill();
+  ctx.drawImage(dots[2], 197, 197);
+}
--- src/script.js
+++ src/script.js
@@ -5,13 +5,8 @@ function init(){
   $.ajax({url: "./src/variables.js", dataType: "script", async: false});
   // 各種キーイベントの用意
   $.ajax({url: "./src/keyevents.js", dataType: "script", async: false});
-  var ctx = getctx();
+  // 描画関数の用意
+  $.ajax({url: "./src/draw.js", dataType: "script", async: false});
   loading();
-  ctx.drawImage(blank, 0, 0);
-  ctx.beginPath();
-  ctx.arrow(200, 400, 200, 0, [0, 1, -10, 1, -10, 5]);
-  ctx.arrow(0, 200, 400, 200, [0, 1, -10, 1, -10, 5]);
-  ctx.fillStyle = "#000";
-  ctx.fill();
-  ctx.drawImage(dots[2], 197, 197);
+  drawTest();
 }
