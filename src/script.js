6c7c53b0541b7417885e703cf43602ab746b9e73
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-06 04:47:32 +0900

    画像のロードをinitの外に出して再挑戦

--- src/script.js
+++ src/script.js
@@ -1,12 +1,21 @@
+
+"use strict";  // これを書くと宣言されていない変数には代入できなくなるので重要
+// 各種変数の宣言
+$.ajax({url: "./src/variables.js", dataType: "script", async: false});
+// 各種キーイベントの用意
+$.ajax({url: "./src/keyevents.js", dataType: "script", async: false});
+// 描画関数の用意
+$.ajax({url: "./src/draw.js", dataType: "script", async: false});
+
 // 初期化。
 function init(){
-  "use strict";  // これを書くと宣言されていない変数には代入できなくなるので重要
-  // 各種変数の宣言
-  $.ajax({url: "./src/variables.js", dataType: "script", async: false});
-  // 各種キーイベントの用意
-  $.ajax({url: "./src/keyevents.js", dataType: "script", async: false});
-  // 描画関数の用意
-  $.ajax({url: "./src/draw.js", dataType: "script", async: false});
   loading();
+  // とりあえずここでドットを描く。441個くらい。
+  // それ以降はShiftキーで状態変化させる感じ。で、Enterで起爆して点を動かす。
+  // Enterの度に最初の状態から・・Resetで戻して。
+  // Enterで点が動き出す、その間操作禁止、動き終わったらResetで戻す（BackSpace, ボタンも作る）
+  // 戻したらまたEnterで動かせるようになる、戻さなくてもShiftで別の状態を作れる、
+  // その場合は自然にResetされる、って感じかなぁ。
+  // あっちのビジュアライザーもEnterボタンとモード明示やった方がいいかもしれない。
   drawTest();
 }
