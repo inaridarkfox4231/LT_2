c308389e6fec2a270995a8571eba500843889918
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-06 10:28:56 +0900

    drawの方にもlog付けて再挑戦

--- src/draw.js
+++ src/draw.js
@@ -1,11 +1,13 @@
 function drawTest(){
   var ctx = getctx();
   console.log('%s %d', "draw", 2)
+  console.log(blank);
   ctx.drawImage(blank, 0, 0);
   ctx.beginPath();
   ctx.arrow(200, 400, 200, 0, [0, 1, -10, 1, -10, 5]);
   ctx.arrow(0, 200, 400, 200, [0, 1, -10, 1, -10, 5]);
   ctx.fillStyle = "#000";
   ctx.fill();
+  console.log(dots[2]);
   ctx.drawImage(dots[2], 197, 197);
 }
